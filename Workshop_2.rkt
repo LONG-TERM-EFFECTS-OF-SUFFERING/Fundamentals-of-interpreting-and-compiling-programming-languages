82b3b68ba5b81fb043c475b93db1846f88fdd0d0
Br4z <67658575+Br4z@users.noreply.github.com>
2023-09-23 15:25:52 -0500

    Add second point and started the third one

--- Workshop_2.rkt
+++ Workshop_2.rkt
@@ -1,9 +1,13 @@
+; -------------------------------------------------------------------------- ;
+;                              1. SAT INSTANCES                              ;
+; -------------------------------------------------------------------------- ;
+
 ; ---------------------------- 1.1 Gramática BNF --------------------------- ;
 
 ; <FNC>             := <number> <logic operation>
 ; <logic operation> := OR (<logic operation>  <logic operation>)
 ; <logic operation> := AND (<logic operation>  <logic operation>)
-; <logic operation> := <boolean value>
+; <logic operation> := <boolean var>
 
 
 ; -------------------------------------------------------------------------- ;
@@ -30,13 +34,52 @@
 	)
 ))
 
+(define boolean_var-list (
+	lambda (number) (
+		list 'BOOLEAN number
+	)
+))
+
+; ------------------------------- PREDICATES ------------------------------- ;
+
+(define fnc-list? (
+	lambda (exp) (
+		eqv? (car exp) 'FNC
+	)
+))
+
+(define or-list? (
+	lambda (exp) (
+		eqv? (car exp) 'OR
+	)
+))
+
+(define and-list? (
+	lambda (exp) (
+		eqv? (car exp) 'AND
+	)
+))
+
+(define boolean_var-list? (
+	lambda (exp) (
+		eqv? (car exp) 'BOOLEAN
+	)
+))
+
 ; ------------------------------- EXTRACTORS ------------------------------- ;
 
-(define fnc->vars (
+(define fnc-list->vars (
 	lambda (fnc-list) (
 		cadr fnc-list
 	)
 ))
+
+(define fnc-list->logic-operation (
+	lambda (fnc-list) (
+		caddr fnc-list
+	)
+))
+
 
 (define or-list->rand1 (
 	lambda (or-list) (
@@ -62,21 +105,122 @@
 	)
 ))
 
+(define boolean_var-list->number (
+	lambda (boolean_var-list) (
+		cadr boolean_var-list
+	)
+))
 
-; -------------------------------- EXEMPLES -------------------------------- ;
-
+; ----------------------- EXAMPLES LIST IMPLEMENTATION --------------------- ;
 
 ; (X OR NOT y OR z OR w) AND (NOT x OR NOT y OR NOT z) x = 1, y = 2, z = 3 and w = 4
 
-(define expression1 (fnc-list 4
+(define list-fnc-expression (fnc-list 4
 	(
 		and-list
-			(or-list (or-list (or-list 1 -2) 3) 4)
-			(or-list (or-list -1 -2) -3)
+			(or-list (or-list (or-list (boolean_var-list 1) (boolean_var-list -2)) (boolean_var-list 3)) (boolean_var-list 4))
+			(or-list (or-list (boolean_var-list -1) (boolean_var-list -2)) (boolean_var-list -3))
+	)
+))
+
+; -------------------------------------------------------------------------- ;
+;                          DATA TYPE IMPLEMENTATION                          ;
+; -------------------------------------------------------------------------- ;
+
+(define-datatype logic_operation logic_operation?
+	(boolean-var-exp (value number?))
+	(or-exp (rand1 logic_operation?) (rand2 logic_operation?))
+	(and-exp (rand1 logic_operation?) (rand2 logic_operation?))
+)
+
+(define-datatype fnc fnc?
+	(fnc-exp (vars number?) (logic-operation logic_operation?))
+)
+
+; -------------------- EXAMPLE DATA TYPES IMPLEMENTATION ------------------- ;
+
+; (X OR NOT y OR z OR w) AND (NOT x OR NOT y OR NOT z) x = 1, y = 2, z = 3 and w = 4
+
+(define dataype-fnc-expression (fnc-exp 5
+	(
+		and-exp
+			(or-exp (or-exp (or-exp (boolean-var-exp 1) (boolean-var-exp -2)) (boolean-var-exp 3)) (boolean-var-exp 4))
+			(or-exp (or-exp (boolean-var-exp -1) (boolean-var-exp -2)) (boolean-var-exp -3))
+	)
+))
+
+; -------------------------------------------------------------------------- ;
+;                        2. PARSE Y UNPARSE FUNCTIONS                        ;
+; -------------------------------------------------------------------------- ;
+
+; ----------------------------- LOGIC OPERATION ---------------------------- ;
+
+(define unparse-logic_operation-expression (
+	lambda (exp) (
+		cases logic_operation exp
+			(boolean-var-exp (value) (boolean_var-list value))
+			(or-exp (rand1 rand2) (or-list
+				(unparse-logic_operation-expression rand1)
+				(unparse-logic_operation-expression rand2)))
+			(and-exp (rand1 rand2) (and-list
+				(unparse-logic_operation-expression rand1)
+				(unparse-logic_operation-expression rand2)))
 	)
 ))
 
 
+(define parse-logic_operation-expresion (
+	lambda (exp) (
+		cond
+			[(boolean_var-list? exp) (boolean-var-exp (boolean_var-list->number exp))]
+			[(or-list? exp)
+				(or-exp (parse-logic_operation-expresion (or-list->rand1 exp))
+						(parse-logic_operation-expresion (or-list->rand2 exp)))]
+			[(and-list? exp)
+				(and-exp (parse-logic_operation-expresion (and-list->rand1 exp))
+						(parse-logic_operation-expresion (and-list->rand2 exp)))]
+	)
+))
+
+; ----------------------------------- FNC ---------------------------------- ;
+
+(define unparse-fnc-expression (
+	lambda (exp) (
+		cases fnc exp
+			(fnc-exp (vars bolean-operation) (list 'FNC vars (unparse-logic_operation-expression bolean-operation)))
+	)
+))
+
+(define parse-fnc-expresion (
+	lambda (exp) (
+		cond
+			[(fnc-list? exp) (fnc-exp
+				(fnc-list->vars exp)
+				(parse-logic_operation-expresion (fnc-list->logic-operation exp)))]
+	)
+))
+
+
+; (unparse-fnc-expression dataype-fnc-expression)
+; (parse-fnc-expresion (unparse-fnc-expression dataype-fnc-expression))
+
 ; -------------------------------------------------------------------------- ;
-;                          DATA TYPES IMPLEMENTATION                         ;
+;                       3. EVALUATION OF SAT INSTANCES                       ;
 ; -------------------------------------------------------------------------- ;
+
+(define evaluate-proposal (
+	lambda (logic-operation proposal) (
+		cases logic_operation logic-operation
+			(boolean-var-exp (value) (
+				if (> value 0)
+					(nth-element proposal (- value 1))
+					(* -1 (nth-element proposal (- value 1)))
+			))
+			(or-exp (rand1 rand2) (or
+				(evaluate-proposal rand1 proposal)
+				(evaluate-proposal rand2 proposal)))
+			(and-exp (rand1 rand2) (and
+				(evaluate-proposal rand1 proposal)
+				(evaluate-proposal rand2 proposal)))
+	)
+))
