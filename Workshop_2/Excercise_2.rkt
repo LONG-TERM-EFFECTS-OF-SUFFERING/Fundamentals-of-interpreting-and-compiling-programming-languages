9e5e7282d6561693b96095c9e9af6d8f82d32ccd
Br4z <67658575+Br4z@users.noreply.github.com>
2023-09-27 14:15:14 -0500

    Add new structure and grammar

---
+++ Workshop_2/Excercise_2.rkt
@@ -0,0 +1,53 @@
+; Calderón Prieto Brandon (2125874)
+; Corrales Carlos Daniel (2122878)
+; Melo Burbano Deisy (2041790)
+
+#lang eopl
+
+; ----------------------------------- OR ----------------------------------- ;
+
+(define unparse-or-expression (
+	lambda (exp) (
+
+	)
+))
+
+
+(define parse-or-expresion (
+	lambda (exp) (
+
+	)
+))
+
+; ----------------------------------- AND ---------------------------------- ;
+
+(define unparse-and-expression (
+	lambda (exp) (
+
+	)
+))
+
+
+(define parse-and-expresion (
+	lambda (exp) (
+
+	)
+))
+
+; ----------------------------------- FNC ---------------------------------- ;
+
+(define unparse-fnc-expression (
+	lambda (exp) (
+
+	)
+))
+
+(define parse-fnc-expresion (
+	lambda (exp) (
+
+	)
+))
+
+
+; (unparse-fnc-expression dataype-fnc-expression)
+; (parse-fnc-expresion (unparse-fnc-expression dataype-fnc-expression))
---
+++ Workshop_2/Excercise_3.rkt
@@ -0,0 +1,5 @@
+; Calderón Prieto Brandon (2125874)
+; Corrales Carlos Daniel (2122878)
+; Melo Burbano Deisy (2041790)
+
+#lang eopl
